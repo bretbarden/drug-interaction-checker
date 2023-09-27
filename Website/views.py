f88af101549df1c1662d4c7d0e75cfc4c8215278
Zulai Romero <zulairomero@gmail.com>
2023-09-27 15:40:10 -0400

    configure api requests

--- Website/views.py
+++ Website/views.py
@@ -1,6 +1,7 @@
 # Store standard roots in Wesbite (main directory)
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+import requests
 # from .models import Bike
 from . import db
 
@@ -10,8 +11,28 @@ views = Blueprint("views", __name__)
 @login_required
 def home():
     print ("Hi I am a route")
+    if request.method == "POST":
+        medicationA = request.form.get("medicationA").strip() if request.form.get("medicationA") else None
+        medicationB = request.form.get("medicationB").strip() if request.form.get("medicationB") else None
+
+        print(medicationA, medicationB, "I WORK YAY")
+
+        responseA = requests.get( f"https://api.fda.gov/drug/label.json/?api_key=1tBoJ0npQzVMLDVsMWgzHVqySLpyrzSyfGk8EhsO&search=openfda.generic_name:{medicationA}").json()["results"][0]["drug_interactions"][0]
+
+
+
+        responseB = requests.get(f"https://api.fda.gov/drug/label.json/?api_key=1tBoJ0npQzVMLDVsMWgzHVqySLpyrzSyfGk8EhsO&search=openfda.generic_name:{medicationB}").json()["results"][0]["drug_interactions"][0]
+
+        return f"{responseA} + 'FIILLER FILLER FILLER' {responseB}"
+
+
+      
+
 
     return render_template("home.html", user=current_user)
+
+
+
 
 
 
